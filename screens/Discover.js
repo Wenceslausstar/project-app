4fc6c9eb28aaa5e65c5a0de74446273953bef18a
wesley <wesleywenceslaus07@gmail.com>
2023-11-28 13:36:49 +0300

    apis

--- screens/Discover.js
+++ screens/Discover.js
@@ -5,24 +5,34 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { useNavigation } from "@react-navigation/native";
-import { Avatar, Hotels } from "../assets";
+import { Attractions, Avatar, Hotels, NotFound, Restaurants } from "../assets";
 import MenuContainer from "../components/MenuContainer";
-import { MaterialIcons } from "@expo/vector-icons";
+
+import { FontAwesome } from "@expo/vector-icons";
 import ItemCarDontainer from "../components/ItemCarDontainer";
+import { getPlacesData } from "../api";
 
 const Discover = () => {
   const navigation = useNavigation();
-  /* React.useEffect(() => {
+
+  const [type, setType] = useState("restaurants");
+  const [isLoading, setIsLoading] = useState(false);
+  const [mainData, setMainData] = useState([]);
+  const [bl_lat, setBl_lat] = useState(null);
+  const [bl_lng, setBl_lng] = useState(null);
+  const [tr_lat, setTr_lat] = useState(null);
+  const [tr_lng, setTr_lng] = useState(null);
+
+  useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
-  }, []);*/
-
-  const [type, setType] = useState("restaurants");
+  }, []);
 
   useEffect(() => {
     setIsLoading(true);
@@ -34,30 +44,23 @@ const Discover = () => {
     });
   }, [bl_lat, bl_lng, tr_lat, tr_lng, type]);
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, []);
   return (
-    <SafeAreaView className="flex-1 bg-white relative mt-8">
+    <SafeAreaView className="flex-1 bg-white relative">
       <View className="flex-row items-center justify-between px-8">
         <View>
-          <Text className="text-[40px] text-[#009bcc] font-bold ">
-            Discover
-          </Text>
-          <Text className="text-[#6ad8c0] text-[33px]">
-            the magic of nature.!
-          </Text>
+          <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527283] text-[36px]">the beauty today</Text>
         </View>
-        <View className="w-12 h-12 bg-gray-600 rounded-md items-center justify-center">
+
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-lg">
           <Image
             source={Avatar}
-            className="w-full h-full rounded-md object-cover  "
+            className="w-full h-full rounded-md object-cover"
           />
         </View>
       </View>
-      <View className="flex-row items-center bg-[#e0d9d9f3] mx-4 rounded-xl py-1 px-0 shadow-lg mt-4">
+
+      <View className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 shadow-lg mt-4">
         <GooglePlacesAutocomplete
           GooglePlacesDetailsQuery={{ fields: "geometry" }}
           placeholder="Search"
@@ -65,13 +68,18 @@ const Discover = () => {
           onPress={(data, details = null) => {
             // 'details' is provided when fetchDetails = true
             console.log(details?.geometry?.viewport);
+            setBl_lat(details?.geometry?.viewport?.southwest?.lat);
+            setBl_lng(details?.geometry?.viewport?.southwest?.lng);
+            setTr_lat(details?.geometry?.viewport?.northeast?.lat);
+            setTr_lng(details?.geometry?.viewport?.northeast?.lng);
           }}
           query={{
-            key: "YOUR API KEY",
+            key: "YOUR_API_KEY",
             language: "en",
           }}
         />
       </View>
+
       {/* Menu Container */}
       {isLoading ? (
         <View className=" flex-1 items-center justify-center">
