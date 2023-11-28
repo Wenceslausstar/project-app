2691343001c8832a969ee0a97afae009a7db4201
wesley <wesleywenceslaus07@gmail.com>
2023-11-28 13:17:21 +0300

    react app

---
+++ components/MenuContainer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { TouchableOpacity, View, Image, Text, StyleSheet } from "react-native";
+
+const MenuContainer = ({ title, imageSrc, type, setType }) => {
+  const handlePress = () => {
+    setType(title.toLowerCase());
+  };
+  return (
+    <TouchableOpacity
+      className="items-center justify-center space-y-2"
+      onPress={handlePress}
+    >
+      <View
+        className={`w-24 h-24 p-2 shadow-sm rounded-full items-center justify-center ${
+          type === title.toLowerCase() ? "bg-gray-400" : ""
+        }`}
+      >
+        <Image source={imageSrc} className="w-full h-full object-contain" />
+      </View>
+      <Text className="text-[#00BCC9] text-xl font-semibold">{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default MenuContainer;
--- screens/Discover.js
+++ screens/Discover.js
@@ -1,6 +1,18 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React, { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { useNavigation } from "@react-navigation/native";
+import { Avatar, Hotels } from "../assets";
+import MenuContainer from "../components/MenuContainer";
+import { MaterialIcons } from "@expo/vector-icons";
+import ItemCarDontainer from "../components/ItemCarDontainer";
 
 const Discover = () => {
   const navigation = useNavigation();
@@ -9,24 +21,141 @@ const Discover = () => {
       headerShown: false,
     });
   }, []);*/
+
+  const [type, setType] = useState("restaurants");
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(bl_lat, bl_lng, tr_lat, tr_lng, type).then((data) => {
+      setMainData(data);
+      setInterval(() => {
+        setIsLoading(false);
+      }, 2000);
+    });
+  }, [bl_lat, bl_lng, tr_lat, tr_lng, type]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerShown: true,
+      headerShown: false,
     });
   }, []);
   return (
-    <SafeAreaView className="flex-1 bg-white relative">
+    <SafeAreaView className="flex-1 bg-white relative mt-8">
       <View className="flex-row items-center justify-between px-8">
         <View>
           <Text className="text-[40px] text-[#009bcc] font-bold ">
             Discover
           </Text>
-          <Text className="text-[#6ad8c0] text-[36px]">
+          <Text className="text-[#6ad8c0] text-[33px]">
             the magic of nature.!
           </Text>
         </View>
-        <View className="w-12 h-12 bg-gray-600 rounded-md items-center justify-center"></View>
+        <View className="w-12 h-12 bg-gray-600 rounded-md items-center justify-center">
+          <Image
+            source={Avatar}
+            className="w-full h-full rounded-md object-cover  "
+          />
+        </View>
       </View>
+      <View className="flex-row items-center bg-[#e0d9d9f3] mx-4 rounded-xl py-1 px-0 shadow-lg mt-4">
+        <GooglePlacesAutocomplete
+          GooglePlacesDetailsQuery={{ fields: "geometry" }}
+          placeholder="Search"
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(details?.geometry?.viewport);
+          }}
+          query={{
+            key: "YOUR API KEY",
+            language: "en",
+          }}
+        />
+      </View>
+      {/* Menu Container */}
+      {isLoading ? (
+        <View className=" flex-1 items-center justify-center">
+          <ActivityIndicator size="large" color="#0B646B" />
+        </View>
+      ) : (
+        <ScrollView>
+          <View className=" flex-row items-center justify-between px-8 mt-8">
+            <MenuContainer
+              key={"hotels"}
+              title="Hotels"
+              imageSrc={Hotels}
+              type={type}
+              setType={setType}
+            />
+
+            <MenuContainer
+              key={"attractions"}
+              title="Attractions"
+              imageSrc={Attractions}
+              type={type}
+              setType={setType}
+            />
+
+            <MenuContainer
+              key={"restaurants"}
+              title="Restaurants"
+              imageSrc={Restaurants}
+              type={type}
+              setType={setType}
+            />
+          </View>
+
+          <View>
+            <View className="flex-row items-center justify-between px-4 mt-8">
+              <Text className="text-[#2C7379] text-[28px] font-bold">
+                Top Tips
+              </Text>
+              <TouchableOpacity className="flex-row items-center justify-center space-x-2">
+                <Text className="text-[#A0C4C7] text-[20px] font-bold">
+                  Explore
+                </Text>
+                <FontAwesome
+                  name="long-arrow-right"
+                  size={24}
+                  color="#A0C4C7"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
+              {mainData?.length > 0 ? (
+                <>
+                  {mainData?.map((data, i) => (
+                    <ItemCarDontainer
+                      key={i}
+                      imageSrc={
+                        data?.photo?.images?.medium?.url
+                          ? data?.photo?.images?.medium?.url
+                          : "https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"
+                      }
+                      title={data?.name}
+                      location={data?.location_string}
+                      data={data}
+                    />
+                  ))}
+                </>
+              ) : (
+                <>
+                  <View className="w-full h-[400px] items-center space-y-8 justify-center">
+                    <Image
+                      source={NotFound}
+                      className=" w-32 h-32 object-cover"
+                    />
+                    <Text className="text-2xl text-[#428288] font-semibold">
+                      Opps...No Data Found
+                    </Text>
+                  </View>
+                </>
+              )}
+            </View>
+          </View>
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
